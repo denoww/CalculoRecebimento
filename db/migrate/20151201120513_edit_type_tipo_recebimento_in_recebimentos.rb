bc26f53cf4906ca4fb76389bca0f233e73d7df70
Rodrigo <denoww@gmail.com>
2015-12-01 11:46:24 -0200

    Criado "down" da migration de alterar tipo_recebimento

--- db/migrate/20151201120513_edit_type_tipo_recebimento_in_recebimentos.rb
+++ db/migrate/20151201120513_edit_type_tipo_recebimento_in_recebimentos.rb
@@ -1,5 +1,8 @@
 class EditTypeTipoRecebimentoInRecebimentos < ActiveRecord::Migration
-  def change
+  def up
     change_column :recebimentos, :tipo_recebimento, :boolean
+  end
+  def down
+    change_column :recebimentos, :tipo_recebimento, :string
   end
 end
