ae326ad52d766f6edf558899848d5895699890d1
Rodrigo <denoww@gmail.com>
2015-12-07 12:46:26 -0200

    Enviar email em prod

--- config/application.rb
+++ config/application.rb
@@ -9,17 +9,5 @@ module ReceberCobranca
 
     config.active_record.raise_in_transactional_callbacks = true
 
-
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'gmail.com',
-      user_name:            "teste.seucondominio@gmail.com",
-      password:             "testestaging",
-      authentication:       'plain',
-      enable_starttls_auto: true
-    }
-
   end
 end
